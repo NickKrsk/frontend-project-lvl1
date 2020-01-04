e0f24fe08a4f15ae90db3df4ca8dd39b768d80b3
nick <ggd43@rambler.ru>
2020-01-04 21:32:58 +0700

    1

--- dist/bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _ = require("..");
-
-(0, _.hello)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOztBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiIyEvdXNyL2Jpbi9lbnYgbm9kZVxuaW1wb3J0IHsgaGVsbG8gfSBmcm9tICcuLic7XG5oZWxsbygpO1xuIl19
