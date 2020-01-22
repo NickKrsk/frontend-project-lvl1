645a3a69b7e3bd3da6e46ac86280b4967572ce9e
Nick <30614907+NickKrsk@users.noreply.github.com>
2020-01-22 14:55:03 +0700

    an->currentElement

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,20 +5,20 @@ import getRandom from '../random';
 const gameDescription = 'What number is missing in the progression?';
 const getQuestionAndAnswer = () => {
   const length = 10;
-  const a1 = getRandom(0, 100);
-  const d = getRandom(0, 100);
-  const n = getRandom(0, length);
+  const firstElement = getRandom(0, 100);
+  const diff = getRandom(0, 100);
+  const hiddenElementPosition = getRandom(0, length);
 
   let question = '';
-  let sequenceSum;
+  let currentElement;
   let answer;
   for (let i = 0; i < length; i += 1) {
-    sequenceSum = a1 + d * i;
-    if (i === n) {
-      answer = sequenceSum;
+    currentElement = firstElement + diff * i;
+    if (i === hiddenElementPosition) {
+      answer = currentElement;
       question = `${question} .. `;
     } else {
-      question += `${question} ${sequenceSum}`;
+      question += `${question} ${currentElement}`;
     }
   }
   return cons(question, answer);
